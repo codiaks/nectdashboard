870a3f5d5bbc49a95d413d29a59eb8896d69b0e9
AKHIL SURYA A <54658593+codiaks@users.noreply.github.com>
2024-06-30 14:04:59 +0530

    [FEAT] organize folders and layout (#1)
    make changes to the folder structure and page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { Box, CssBaseline } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Suspense } from "react";
 import { useRoutes } from "react-router";
 import "./App.css";
 import { routes } from "./GlobalRoutes";
 
+const defaultTheme = createTheme();
+
+
 function App() {
   const route = useRoutes(routes);
 
   return (
-    <Box>
-      <CssBaseline />
-      <Suspense fallback={"Loading..."}>{route}</Suspense>
-    </Box>
+    <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Suspense fallback={"Loading..."}>{route}</Suspense>
+    </ThemeProvider>
   );
 }
 
